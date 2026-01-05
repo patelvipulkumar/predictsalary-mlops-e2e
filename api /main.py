b288d0a8a588c03129a2b78ce2e87c5f4bbcc427
patelvipulkumar <247011054+patelvipulkumar@users.noreply.github.com>
2026-01-05 00:04:49 -0500

    Create main.py

---
+++ api /main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+import joblib
+import boto3
+import os
+
+# ---------------------------
+# CHANGE THESE IF NEEDED
+# ---------------------------
+S3_BUCKET = "edu-mlops-awsec2s3"
+S3_KEY = "latest/model.pkl"
+LOCAL_MODEL_PATH = "models/model.pkl"
+# ---------------------------
+
+app = FastAPI()
+
+def download_model():
+    if not os.path.exists(LOCAL_MODEL_PATH):
+        os.makedirs("models", exist_ok=True)
+        s3 = boto3.client("s3")
+        s3.download_file(S3_BUCKET, S3_KEY, LOCAL_MODEL_PATH)
+
+# Download model ONCE at startup
+download_model()
+
+model = joblib.load(LOCAL_MODEL_PATH)
+
+@app.post("/predict")
+def predict(exp: float):   
+    prediction = model.predict(exp)
+    return {"Salary": prediction}
