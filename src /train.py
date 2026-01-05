88d85fe5b250b02da1d0e474960345692cfac549
patelvipulkumar <247011054+patelvipulkumar@users.noreply.github.com>
2026-01-05 00:12:10 -0500

    Create train.py

---
+++ src /train.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
+
+S3_BUCKET = "edu-mlops-awsec2s3"
+S3_KEY = "latest/model.pkl"
+
+
+df = pd.read_csv("data/processed/empexpsalclean.csv")
+X = data[['YearsExperience']] 
+y = data['Salary']
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+ 
+mse = mean_squared_error(y_test, predictions)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, predictions)
+
+with mlflow.start_run():
+    mlflow.log_metric{"MSE": mse, "RMSE": rmse, "R-squared": r2}
+
+joblib.dump(model, "models/model.pkl")
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R-squared:", r2)
+
+
+# Upload to S3 for serving
+s3 = boto3.client("s3")
+s3.upload_file("models/model.pkl", S3_BUCKET, S3_KEY)
+
+print("✅ Model trained and uploaded to S3")
